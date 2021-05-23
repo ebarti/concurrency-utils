e5d0ccb05803d4daf19a114dfd0aab7a27ad9cbd
ebarti <eloi.barti@glovoapp.com>
2021-05-23 10:56:43 +0200

    Add tests

--- utils_test.go
+++ utils_test.go
@@ -1,8 +1,8 @@
 package concurrencyutils
 
 import (
+	"reflect"
 	"testing"
-	"time"
 )
 
 func TestBridge(t *testing.T) {
@@ -19,20 +19,14 @@ func TestBridge(t *testing.T) {
 		}()
 		return chanStream
 	}
-	_ = genVals()
-}
-
-func TestOrDone(t *testing.T) {
-	sig := func(idx int, after time.Duration) <-chan interface{} {
-		c := make(chan interface{})
-		go func() {
-			defer close(c)
-			c <- idx
-			time.Sleep(after)
-		}()
-		return c
+	bridge := Bridge(nil, genVals())
+	var vals []interface{}
+	for v := range bridge {
+		vals = append(vals, v)
 	}
-	_ = sig(1, 1*time.Hour)
+	if len(vals) != 10 {
+		t.Errorf("Bridge test failed. Expected 10 values but received only %d", len(vals))
+	}
 }
 
 func TestTee(t *testing.T) {
@@ -55,5 +49,108 @@ func TestTee(t *testing.T) {
 		c2v = append(c2v, <-c2)
 		c3v = append(c3v, <-c3)
 		c4v = append(c4v, <-c4)
+	}
+	if len(c1v) != 10 {
+		t.Errorf("Tee test failed. Channel 1 expected 10 values but received only %d", len(c1v))
+	}
+	if len(c2v) != 10 {
+		t.Errorf("Tee test failed. Channel 2 expected 10 values but received only %d", len(c2v))
+	}
+	if len(c3v) != 10 {
+		t.Errorf("Tee test failed. Channel 3 expected 10 values but received only %d", len(c3v))
+	}
+	if len(c4v) != 10 {
+		t.Errorf("Tee test failed. Channel 4 expected 10 values but received only %d", len(c4v))
+	}
+}
+
+func TestOrDone(t *testing.T) {
+	type args struct {
+		sendDoneAtIdx int
+		c             []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want []interface{}
+	}{
+		{
+			name: "Test never done",
+			args: args{
+				sendDoneAtIdx: 99,
+				c:             []interface{}{0, 1, 2, 3, 4, 5, 6, 7},
+			},
+			want: []interface{}{0, 1, 2, 3, 4, 5, 6, 7},
+		},
+		{
+			name: "Test done in between",
+			args: args{
+				sendDoneAtIdx: 4,
+				c:             []interface{}{0, 1, 2, 3, 4, 5, 6, 7},
+			},
+			want: []interface{}{0, 1, 2, 3, 4},
+		},
+		{
+			name: "Test done",
+			args: args{
+				sendDoneAtIdx: 0,
+				c:             []interface{}{0, 1, 2, 3, 4, 5, 6, 7},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			done := make(chan interface{})
+			c := make(chan interface{})
+			go func() {
+				defer func() {
+					close(done)
+					close(c)
+				}()
+				for i, v := range tt.args.c {
+					if i == tt.args.sendDoneAtIdx {
+						done <- 1
+						return
+					}
+					c <- v
+				}
+			}()
+			got := OrDone(done, c)
+			if tt.want != nil {
+				i := 0
+				for gotValue := range got {
+					if !reflect.DeepEqual(gotValue, tt.want[i]) {
+						t.Errorf("OrDone() = %v, want %v", gotValue, tt.want[i])
+					}
+					i++
+				}
+			} else {
+				if len(got) > 0 {
+					t.Error("Got values but shouldn't have")
+				}
+			}
+		})
+	}
+}
+
+func TestTake(t *testing.T) {
+	type args struct {
+		done        <-chan interface{}
+		valueStream <-chan interface{}
+		num         int
+	}
+	tests := []struct {
+		name string
+		args args
+		want <-chan interface{}
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Take(tt.args.done, tt.args.valueStream, tt.args.num); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Take() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
